bc3ed1bf9f9d184b5640c119b2ebfebb66299a6e
abdullohn001 <muhammadaligulomov41@gmail.com>
2024-05-10 17:47:42 +0500

    11- dars

---
+++ app.js
@@ -0,0 +1,59 @@
+// 1- masala
+//let numbers = [3,4,5,6,7,];
+//let number = numbers.reverse();
+//console.log(number);
+
+
+//2-masala
+// == bu faqat qiymatlarni taqqoslaydi 
+// === bu esa typ larni ham solishtiradi 
+
+// if(10 == "10"){
+//     console.log(true);
+// } else{
+//     console.log(false)
+// }
+// if (10 === "10") {
+//   console.log(true);
+// } else{
+//     console.log(false)
+// }
+
+//3-masala
+// let son = prompt("raqam kiriting");
+// for(let i = 1; i <= son; i++ ){
+//     console.log([i])
+// }
+
+//4-masala
+// let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let toqSonlar = sonlar.filter((son) => son % 2 !== 0);
+// let juftSonlar = sonlar.filter((son) => son % 2 === 0);
+// let toqSonlarYigindisi = toqSonlar.reduce((yigindi, son) => yigindi + son);
+// let juftSonlarYigindisi = juftSonlar.reduce((yigindi, son) => yigindi + son);
+// console.log("Toq sonlar yig'indisi:", toqSonlarYigindisi);
+// console.log("Juft sonlar yig'indisi:", juftSonlarYigindisi);
+
+
+//5 - masala
+// let inputString = "Hello! World!! How are you?!".split('!').join('');
+// console.log(inputString); 
+
+//6- masala
+// let raqam = [5,4,6,3];
+// let raqam2 = [6,8,9];
+// let SonlarYigindisi = raqam.reduce((yigindi, son) => yigindi + son);
+// let SonlarYigindisi2 = raqam2.reduce((yigindi, son) => yigindi + son);
+// console.log(SonlarYigindisi + SonlarYigindisi2)
+
+
+//7- masala
+// function make(str) {
+//     let word = str.split("");
+//         word.pop();
+//         word.shift();
+//         return word.join("");
+//  }
+
+
+
